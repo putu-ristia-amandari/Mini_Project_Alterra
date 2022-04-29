c0c93b38dbb257dc86a48d7bb2b54bfd51b6a8b5
Putu Ristia Amandari <ristiaamandari@gmail.com>
2022-04-29 16:28:59 +0800

    update4

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -2,15 +2,14 @@ package routes
 
 import (
 	"net/http"
-	
 
 	"github.com/labstack/echo/v4"
 )
 
-func New() e *echo.Echo {
+func New() *echo.Echo {
 	e := echo.New
 
-	e.GET("/", func(c echo.Context) error{
+	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello!")
 	})
 
